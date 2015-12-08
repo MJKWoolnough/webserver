539ecede3c9309c44b8924beece2b3c467a5c888
Michael Woolnough <michael.woolnough@gmail.com>
2015-12-08 16:26:31 +0000

    Rewrote client to use an inherited unix socket to receive data on

--- proxy/client/client.go
+++ proxy/client/client.go
@@ -3,111 +3,91 @@ package client
 import (
 	"encoding/binary"
 	"errors"
-	"io"
 	"net"
 	"os"
+	"syscall"
 )
 
-type addr struct{}
-
-func (addr) Network() string {
-	return "proxy"
+type listener struct {
+	unix *net.UnixConn
 }
 
-func (addr) String() string {
-	return "proxy"
+func Listen(socketFD uintptr) (net.Listener, error) {
+	c, err := net.FileConn(os.NewFile(socketFD, ""))
+	if err != nil {
+		return nil, err
+	}
+	u, ok := c.(*net.UnixConn)
+	if !ok {
+		return nil, ErrInvalidSocket
+	}
+	return &listener{unix: u}, nil
+}
+
+func (l *listener) Accept() (net.Conn, error) {
+	length := make([]byte, 4)
+	_, err := l.unix.Read(length)
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, binary.LittleEndian.Uint32(length))
+	_, err = l.unix.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	oob := make([]byte, syscall.CmsgSpace(4))
+
+	_, _, _, _, err = l.unix.ReadMsgUnix(nil, oob)
+	if err != nil {
+		return nil, err
+	}
+	msg, err = syscall.ParseSocketControlMessage(oob)
+	if err != nil {
+		return nil, err
+	}
+	if len(msg) != 1 {
+		return nil, ErrInvalidSCM
+	}
+	fd, err := syscall.ParseUnixRights(&msgs[0])
+	if err != nil {
+		return nil, err
+	}
+	if len(fd) != 1 {
+		return nil, ErrInvalidFDs
+	}
+	c, err := net.FileConn(os.NewFile(uintptr(fd[0]), ""))
+	if err != nil {
+		return nil, err
+	}
+	return &conn{
+		buf: buf,
+		Conn, c,
+	}, nil
 }
 
 type conn struct {
-	net.Conn
-	buf []byte
+	buf  []byte
+	Conn net.Conn
 }
 
 func (c *conn) Read(b []byte) (int, error) {
-	if len(c.buf) == 0 {
+	if c.buf == nil {
 		return c.Conn.Read(b)
 	}
 	n := copy(b, c.buf)
 	c.buf = c.buf[n:]
 	if len(c.buf) == 0 {
 		c.buf = nil
-	}
-	if n < len(b) {
 		m, err := c.Conn.Read(b[n:])
 		return n + m, err
 	}
 	return n, nil
 }
 
-type listener struct {
-	data <-chan *conn
-}
-
-func (l *listener) Accept() (net.Conn, error) {
-	c, ok := <-l.data
-	if !ok {
-		return nil, &net.OpError{
-			Op:     "accept",
-			Net:    "proxy",
-			Source: addr{},
-			Addr:   addr{},
-			Err:    ErrClosing,
-		}
-	}
-	return c, nil
-}
-
-func (l *listener) Close() error {
-	return nil
-}
-
-func (l *listener) Addr() net.Addr {
-	return addr{}
-}
-
-func New(r io.Reader) (net.Listener, net.Listener) {
-	nc := make(chan *conn)
-	ec := make(chan *conn)
-
-	go run(r, nc, ec)
-
-	return &listener{nc}, &listener{ec}
-}
-
-func run(r io.Reader, nc, ec chan *conn) {
-	var (
-		connType [1]byte
-		fd       [8]byte
-		length   [4]byte
-		err      error
-	)
-	for {
-		_, err = r.Read(connType[:])
-		_, err = r.Read(fd[:])
-		_, err = r.Read(length[:])
-		buf := make([]byte, int(binary.LittleEndian.Uint32(length[:])))
-		_, err = r.Read(buf)
-		if err != nil {
-			continue
-		}
-
-		var fc net.Conn
-
-		fc, err = net.FileConn(os.NewFile(uintptr(binary.LittleEndian.Uint64(fd[:])), ""))
-
-		c := &conn{
-			Conn: fc,
-			buf:  buf,
-		}
-
-		if connType[0] == 0 {
-			nc <- c
-		} else {
-			ec <- c
-		}
-	}
-
-}
-
 // Errors
-var ErrClosing = errors.New("use of closed network connection")
+var (
+	ErrInvalidSocket = errors.New("invalid socket type")
+	ErrInvalidSCM    = errors.New("invalid number of socket control messages")
+	ErrInvalidFDs    = errors.New("invalid number of file descriptors")
+)
