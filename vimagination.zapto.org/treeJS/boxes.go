7eb067475d45ac405f4dcb283bf417fe5664a225
Michael Woolnough <michael.woolnough@gmail.com>
2017-04-26 21:24:01 +0100

    first attempt at box placement code

--- vimagination.zapto.org/treeJS/boxes.go
+++ vimagination.zapto.org/treeJS/boxes.go
@@ -10,12 +10,21 @@ func (r *Rows) GetRow(row int) int {
 }
 
 func (r *Rows) RowPP(row int) int {
-	for len(*r) <= row {
-		*r = append(*r, make(Rows, row+1-len(*r))...)
-	}
+	r.size(row)
 	c := (*r)[row]
 	(*r)[row]++
 	return c
+}
+
+func (r *Rows) SetRow(row, col int) {
+	r.size(row)
+	(*r)[row] = col
+}
+
+func (r *Rows) size(row int) {
+	for len(*r) <= row {
+		*r = append(*r, make(Rows, row+1-len(*r))...)
+	}
 }
 
 func (r *Rows) Reset() {
@@ -39,6 +48,16 @@ func (b *Box) FirstEmpty() {
 	}
 }
 
+func (b *Box) SetCol(col int) {
+	if rows.GetRow(b.Row) < col {
+		rows.SetRow(b.Row, col)
+	}
+}
+
+func (b *Box) AddCol(diff int) {
+	b.SetCol(b.Col + diff)
+}
+
 type Children struct {
 	Parents  *Spouse
 	Children []Child
@@ -53,11 +72,23 @@ func NewChildren(f *Family, parents *Spouse, row int) Children {
 	for n, child := range children {
 		c.Children[n] = NewChild(child, &c, row)
 	}
+	if parents != nil {
+		c.Shift(diff)
+	}
 	return c
 }
 
-func (c *Children) Shift() {
-
+func (c *Children) Shift(diff int) bool {
+	if len(c.Children) > 0 {
+		if pDiff := parents.Col + diff - 1 - c.Children[len(c.Children)-1].LastX(); pDiff > 0 {
+			for i := len(c.Children) - 1; c >= 0; c-- {
+				if !c.Children[i].Shift(pDiff) {
+					return false
+				}
+			}
+		}
+	}
+	return true
 }
 
 type Child struct {
@@ -77,6 +108,25 @@ func NewChild(p *Person, siblings *Children, row int) Child {
 		c.Spouses = NewSpouses(p.SpouseOf(), &c, row)
 	}
 	return c
+}
+
+func (c *Child) LastX() int {
+	if len(c.Spouses.Spouses) > 0 {
+		return c.Spouses.Spouses[len(c.Spouses.Spouses)-1].Col
+	}
+	return c.Col
+}
+
+func (c *Child) Shift(diff int) bool {
+	if !c.Spouses.Shift(diff) {
+		return false
+	}
+	if len(s.Spouses) > 0 {
+		s.Spouse.Col = s.Spouses[0].Col - 1
+	} else {
+		c.AddCol(diff)
+	}
+	return true
 }
 
 type Spouses struct {
@@ -101,11 +151,13 @@ func NewSpouses(families []*Family, spouse *Child, row int) Spouses {
 	}
 	return s
 }
-
-func (s *Spouses) Shake() {
-	for _, spouse := range s.Spouses {
-		s.Shake()
+func (s *Spouses) Shift(diff int) bool {
+	for i := len(c.Spouses.Spouses) - 1; i >= 0; i-- {
+		if !c.Spouses.Spouses[i].Shift(diff) {
+			return false
+		}
 	}
+	return true
 }
 
 type Spouse struct {
@@ -122,10 +174,19 @@ func NewSpouse(f *Family, p *Person, spouses *Spouses, row int) Spouse {
 		Box:     NewBox(row),
 	}
 	s.Children = NewChildren(f, &s, row+1)
+	if len(f.ChildrenIDs) > 0 {
+		lastChildPos := s.Children.Children[len(s.Children.Children)-1].Col
+		if s.Col+1 > lastChildPos {
+			s.SetCol(lastChildPos + 1)
+		}
+	}
 	return s
 }
 
-func (s *Spouse) Shake() {
-	s.FirstEmpty()
-	s.Children.Shake()
+func (s *Spouse) Shift(diff int) bool {
+	if !s.Children.Shift(diff) {
+		return false
+	}
+	s.AddCol(diff)
+	return true
 }
