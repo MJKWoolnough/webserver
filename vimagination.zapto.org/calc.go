12db066ec1c2410c5c7e5ed6063c70ee536e21a4
Michael Woolnough <michael.woolnough@gmail.com>
2017-05-02 10:38:26 +0100

    rewrote relationship name gen

--- vimagination.zapto.org/calc.go
+++ vimagination.zapto.org/calc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strconv"
+	"strings"
 )
 
 type person struct {
@@ -18,16 +19,98 @@ type Links struct {
 func (l Links) Relationship() string {
 	up := len(l.First)
 	down := len(l.Second)
-	diff := up - down
-	if diff < 0 {
-		diff = -diff
+	var relationship string
+	if up > 0 && down > 0 {
+		if l.First[len(l.First)-1].ChildOf != l.Second[len(l.Second)-1].ChildOf {
+			relationship = "Half-"
+		}
 	}
-	big := up
-	if down > up {
-		big = down
+	switch up {
+	case 0:
+		switch down {
+		case 0:
+			return "Clone"
+		case 1:
+		default:
+			relationship = strings.Repeat("Great-", down-2) + "Grand-"
+		}
+		switch l.Common.Gender {
+		case 'M':
+			relationship += "Father"
+		case 'F':
+			relationship += "Mother"
+		default:
+			relationship += "Parent"
+		}
+	case 1:
+		switch down {
+		case 0:
+			switch l.First[0].Gender {
+			case 'M':
+				relationship = "Son"
+			case 'F':
+				relationship = "Daughter"
+			default:
+				relationship = "Child"
+			}
+		case 1:
+			switch l.First[0].Gender {
+			case 'M':
+				relationship += "Brother"
+			case 'F':
+				relationship += "Sister"
+			default:
+				relationship += "Sibling"
+			}
+		default:
+			relationship += strings.Repeat("Great-", down-2)
+			switch l.First[0].Gender {
+			case 'M':
+				relationship += "Uncle"
+			case 'F':
+				relationship += "Aunt"
+			default:
+				relationship += "Pibling"
+			}
+		}
+	default:
+		switch down {
+		case 0:
+			relationship = strings.Repeat("Great-", up-2) + "Grand-"
+			switch l.First[0].Gender {
+			case 'M':
+				relationship += "Son"
+			case 'F':
+				relationship += "Daughter"
+			default:
+				relationship += "Child"
+			}
+		case 1:
+			relationship += strings.Repeat("Great-", up-2)
+			switch l.First[0].Gender {
+			case 'M':
+				relationship += "Nephew"
+			case 'F':
+				relationship += "Neice"
+			default:
+				relationship += "Nibling"
+			}
+		default:
+			var small, diff int
+			if up > down {
+				small = down - 1
+				diff = up - down
+			} else {
+				small = up - 1
+				diff = down - up
+			}
+			relationship += strconv.Itoa(small) + ordinal(small) + " cousin"
+			if diff > 0 {
+				relationship += ", " + removed(diff) + " removed,"
+			}
+		}
 	}
-	diff++
-	return strconv.Itoa(big) + ordinal(big) + " cousin, " + strconv.Itoa(diff) + " times removed"
+	return relationship
 }
 
 func ordinal(num int) string {
@@ -46,6 +129,18 @@ func ordinal(num int) string {
 			return "th"
 		}
 	}
+}
+
+func removed(diff int) string {
+	switch diff {
+	case 1:
+		return "once"
+	case 2:
+		return "twice"
+	case 3:
+		return "thrice"
+	}
+	return strconv.Itoa(diff) + " times"
 }
 
 func Calculate(first, second *Person) Links {
@@ -93,10 +188,10 @@ func Calculate(first, second *Person) Links {
 func makeRoute(p *person) []*Person {
 	route := make([]*Person, 0, 32)
 	for {
-		route = append(route, p.Person)
-		if p.Person == p.side {
+		if p == nil {
 			break
 		}
+		route = append(route, p.Person)
 		p = p.from
 	}
 	reverse(route)
